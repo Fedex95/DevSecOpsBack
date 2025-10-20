6dfb75ae817c96510831908599dd4ec2821b7ce8
Fedex95 <fedex95@outlook.es>
2025-10-20 18:46:47 -0500

    First

---
+++ KpbackApplication.java
@@ -0,0 +1,12 @@
+package com.tienda.kpback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpbackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KpbackApplication.class, args);
+    }
+
+}
