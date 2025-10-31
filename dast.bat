f8b5085399c046aa635d59a1168ad91c4992c394
Fedex95 <fedex95@outlook.es>
2025-10-31 17:08:08 -0500

    arg3

--- dast.bat
+++ dast.bat
@@ -6,7 +6,11 @@ if "%REPO_DIR%"=="" set "REPO_DIR=%CD%"
 
 cd /d "%ZAP_DIR%"
 
+set "ZAP_HOME=%TEMP%\zap_home_%RANDOM%"
+mkdir "%ZAP_HOME%" 2>nul
+
 call zap.bat -cmd ^
+  -dir "%ZAP_HOME%" ^
   -autorun "%REPO_DIR%\policy.yaml" ^
   -quickurl %ZAP_URL% ^
   -quickout "%REPO_DIR%\report.html"
