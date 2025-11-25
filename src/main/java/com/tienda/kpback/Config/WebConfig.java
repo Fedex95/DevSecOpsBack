2a5aec0686b8ab69268ec3f67a78a51742bf98aa
Fedex95 <fedex95@outlook.es>
2025-11-24 22:09:43 -0500

    fixing dependencies

---
+++ src/main/java/com/tienda/kpback/Config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.tienda.kpback.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${IP_FRONT}")
+    private String IP_FRONT;
+
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://" + IP_FRONT, "http://" + IP_FRONT + ":80", "http://localhost:3000") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization", "X-Requested-With", "Accept")
+                .allowCredentials(true);
+    }
+}
