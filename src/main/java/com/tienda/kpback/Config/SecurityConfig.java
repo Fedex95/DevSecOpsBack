e4c47e0cebf7854140a0ce4852dd8a3d204d3c32
Fedex95 <fedex95@outlook.es>
2025-11-25 20:20:51 -0500

    fixing

--- src/main/java/com/tienda/kpback/Config/SecurityConfig.java
+++ src/main/java/com/tienda/kpback/Config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
                 .requestMatchers(
                     "/api/auth/login",
                     "/api/auth/register",
+                    "/api/usuario/viewPass/**",
                     "/v3/api-docs/**",
                     "/swagger-ui/**",
                     "/swagger-ui.html"
